53a0b10fb48eda7efc97c8c715310995ed0fcf9e
Daniel-Georgiev <daniko91@mail.bg>
2015-05-21 14:29:40 +0300

    comments are now added with enter

--- CourseWork/js/controllers/postsController.js
+++ CourseWork/js/controllers/postsController.js
@@ -27,3 +27,17 @@ app.controller('PostsController', function PostsController($scope, postServices,
     }
 
 });
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
