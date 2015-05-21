ce497afb9cb3c954f4ef14df23214b2426405523
Daniel-Georgiev <daniko91@mail.bg>
2015-05-21 14:00:03 +0300

    dynamic scrolling fixed, comment author added

--- CourseWork/js/services/scrollService.js
+++ CourseWork/js/services/scrollService.js
@@ -1,23 +1,29 @@
 // Reddit constructor function to encapsulate HTTP and pagination logic
 app.factory('SocialNetworkPosts', function($http, baseServiceUrl) {
     var SocialNetworkPosts = function() {
-        this.posts = [];
+        this.socialNetWorkPosts = [];
         this.busy = false;
         this.lastIndex = '';
+        this.loadingMessage= 'Loading posts....';
     };
 
     SocialNetworkPosts.prototype.nextPage = function() {
         if (this.busy) return;
         this.busy = true;
-        var url = baseServiceUrl+ 'me/feed?StartPostId=' + this.lastIndex + '&PageSize=7';
+        var url = baseServiceUrl+ 'me/feed?StartPostId=' + this.lastIndex + '&PageSize=5';
 
         $http.get(url).success(function(data) {
-            console.log(data);
-            var posts = data;
-            for (var i = 0; i < posts.length; i++) {
-                this.posts.push(posts[i]);
+
+            for (var i = 0; i < data.length; i++) {
+                this.socialNetWorkPosts.push(data[i]);
             }
-            this.lastIndex = this.posts[this.posts.length-1].id;
+
+            if (data.length == 0) {
+                this.loadingMessage = 'No more posts.';
+                return;
+            }
+
+            this.lastIndex = this.socialNetWorkPosts[this.socialNetWorkPosts.length-1].id;
             this.busy = false;
         }.bind(this));
 
