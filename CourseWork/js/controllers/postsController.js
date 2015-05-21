315b62d7d21facccf0c29b15532cf7e11a71f6d2
Daniel-Georgiev <daniko91@mail.bg>
2015-05-21 14:55:30 +0300

    moved ngEnter directive

--- CourseWork/js/controllers/postsController.js
+++ CourseWork/js/controllers/postsController.js
@@ -27,17 +27,3 @@ app.controller('PostsController', function PostsController($scope, postServices,
     }
 
 });
-
-app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
-});
